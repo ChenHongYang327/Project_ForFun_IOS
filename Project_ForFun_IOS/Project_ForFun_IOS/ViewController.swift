4822f1291705817d367e98f0b44a4191b9af19d3
陳鋐洋 <ahic2569k3ig@gmail.com>
2021-07-25 12:08:24 +0800

    新建專案

---
+++ Project_ForFun_IOS/Project_ForFun_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_ForFun_IOS
+//
+//  Created by 陳鋐洋 on 2021/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project_ForFun_IOS/Project_ForFun_IOS/ViewController/RegistVC.swift
@@ -0,0 +1,29 @@
+//
+//  RegistVC.swift
+//  Project_ForFun_IOS
+//
+//  Created by 陳鋐洋 on 2021/7/25.
+//
+
+import UIKit
+
+class RegistVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
