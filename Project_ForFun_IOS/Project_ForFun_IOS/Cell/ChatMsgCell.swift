75d7b3c6ee17a285baf3aecfee1f045c7c085e8b
陳鋐洋 <ahic2569k3ig@gmail.com>
2021-08-04 10:10:51 +0800

    更新 chatmsg 部份程式

---
+++ Project_ForFun_IOS/Project_ForFun_IOS/Cell/ChatMsgCell.swift
@@ -0,0 +1,13 @@
+
+
+import UIKit
+
+class ChatMsgCell: UITableViewCell {
+
+    @IBOutlet weak var imgHead: UIImageView!
+    @IBOutlet weak var tvName: UILabel!
+    @IBOutlet weak var tvDate: UILabel!
+    @IBOutlet weak var tvType: UILabel!
+    @IBOutlet weak var tvContent: UITextView!
+    
+}
